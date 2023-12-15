3a8cc7913a56fb14a00e7c56284a441a2d0ad766
Alexey Che <alexey4he@gmail.com>
2023-12-15 17:09:15 +0500

    Init commit

---
+++ src/MainTest.java
@@ -0,0 +1,38 @@
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//
+//import static org.junit.Assert.*;
+//
+//public class MainTest {
+//
+//    @Test
+//    public void main() {
+//        ArrayList<SendMoreMoney2> initialPopulation = new ArrayList<>();
+//        final int POPULATION_SIZE = 1000;
+//        final int GENERATIONS = 1000;
+//        final double THRESHOLD = 1.0;
+//        for (int i = 0; i < POPULATION_SIZE; i++) {
+//            initialPopulation.add(SendMoreMoney2.randomInstance());
+//        }
+//        GeneticAlgorithm<SendMoreMoney2> ga = new GeneticAlgorithm<>(
+//                initialPopulation,
+//                0.2, 0.7, GeneticAlgorithm.SelectionType.ROULETTE);
+//        SendMoreMoney2 result = ga.run(GENERATIONS, THRESHOLD);
+//        System.out.println(result);
+//        try {
+//            FileWriter writer = new FileWriter("fileForWrite.txt", true);
+//
+//            writer.write(String.valueOf(result)+ "\n");
+//            writer.close();
+//
+//        } catch (IOException e) {
+//            System.out.println("Возникла ошибка во время записи, проверьте данные.");
+//        }
+//        SendMoreMoney2 assertResult = result;
+//        Assert.assertEquals(assertResult, result);
+//    }
+//}
